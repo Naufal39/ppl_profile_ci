c98842e7cec802fe7477b424333124b58e0bdff8
unknown <naufalsetyanto39@gmail.com>
2019-05-15 04:33:23 +0700

    first commit

---
+++ application/controllers/Profile.php
@@ -0,0 +1,17 @@
+<?php 
+class Profile extends CI_Controller{
+    function __construct(){
+        parent::__construct();
+        if($this->session->userdata('logged_in') !== TRUE){
+      redirect('login');
+    }
+        $this->load->model('login_model');
+    }
+
+     function u_pakar(){
+    $this->load->view('pakar/dashboard_view');
+    $this->load->view('pakar/index_u');
+  }
+
+  
+}
